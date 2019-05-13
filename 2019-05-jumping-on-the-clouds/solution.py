3d34008160d22ccc1e21a9432fd8c53ba44eeea5
Will Lifferth <wlifferth@icloud.com>
2019-05-13 12:07:23 -0400

    Solved jumping on the clouds in python

---
+++ 2019-05-jumping-on-the-clouds/solution.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c, k):
+    n = len(c)
+    energy = 99
+    i = k % n 
+    while i != 0:
+        energy -= 1
+        if c[i] == 1:
+            energy -= 2
+        i = (i + k) % n
+    if c[i] == 1:
+        energy -= 2
+    return energy
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
