f4bdfaa330b136fd523b987b002175de8138d592
Will Lifferth <wlifferth@icloud.com>
2019-05-12 23:00:43 -0400

    Solved save the prisoner in python

---
+++ 2019-05-save-the-prisoner/solution.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the saveThePrisoner function below.
+def saveThePrisoner(n, m, s):
+    # Subtracting 2 and adding back in 1 to account for zero indexing
+    # Modding n to account for wraparound
+    return (s + m - 2) % n + 1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        nms = input().split()
+
+        n = int(nms[0])
+
+        m = int(nms[1])
+
+        s = int(nms[2])
+
+        result = saveThePrisoner(n, m, s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
+
